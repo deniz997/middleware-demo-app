ef30e66166b7e31593abdb7fe5c4cc6cb738f877
Deniz Mert Tecimer <deniz.tecimer@piworks.net>
2020-09-10 15:26:37 +0300

    Initial commit

---
+++ MiddlewareApp/Middleware/ExternalMiddleware.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace MiddlewareApp.Middleware
+{
+    public class ExternalMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public ExternalMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            await _next(httpContext);
+           
+            await httpContext.Response.WriteAsync("<h5>Hello from external middleware</h5>");
+        }
+    }
+
+    public static class AppBuilderExtensions
+    {
+        public static void UseExternalMiddleware(this IApplicationBuilder app){
+            app.UseMiddleware<ExternalMiddleware>();
+        }
+    }
+}
